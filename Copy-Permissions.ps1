8e40285362d85c91dfd0014673bc7d4aa790ca20
ertugrulblt <56775055+ertugrulblt@users.noreply.github.com>
2019-11-09 23:56:54 +0300

    Add files via upload

---
+++ Copy-Permissions.ps1
@@ -0,0 +1,25 @@
+﻿Param
+(
+[String][Parameter(Mandatory=$true)]$SourcePath,
+[String][Parameter(Mandatory=$true)]$TargetPath,
+[Switch]$LogEnabled
+)
+
+$list=gci "$SourcePath" -recurse
+foreach ($item in $list)
+{
+    $itemAcl=get-acl $item.PSPath 
+    $AlteredItemPath=$item.PSPath.ToString() -replace "$SourcePath", "$TargetPath"
+    if (Test-Path $AlteredItemPath)
+    {
+        Set-Acl -Path $AlteredItemPath -AclObject $itemAcl
+        if ($LogEnabled)
+        {
+            Write-Host "$($item.PSPath.ToString()) folder permissions replicated to $AlteredItemPath" -ForegroundColor Cyan    
+        }
+    }else
+    {
+        Write-Host "$AlteredItemPath does not exist" -ForegroundColor Red
+    }
+}
+
